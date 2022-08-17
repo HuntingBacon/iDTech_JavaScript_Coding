04080f8b187503e25dcb2e5aa5fc245dd57270fc
HuntingBacon <hmp20886@gmail.com>
2022-08-17 11:56:41 -0400

    Added all of my previously finished work.

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,40 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let greating = "welcome you to UA High School.";
+let name = "Roronoa Zoro";
+let title = "Pirate Hunter Zoro";
+let worldname= "Earth";
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let age = 21;
+let year = 2022;
+let century = 21;
+
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let weaponUsed = ['Wado Ichimonji', 'Sandai Kitetsu', 'Enma', 'Seppuku Blade', 'Armament Haki', 'Observation Haki', "Conqueror's Haki"];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let hasStrongestWeapon = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log( name + " I am All Might and I have come to meet you. It is my duty to " + greating + "." );
+console.log("Zoro: Hello All Might. I am Roronoa Zoro of the Strawhat Pirates. Do you happen to know where excactly I am, and What is UA High School.")
+console.log("Also do you happen to know where Luffy is.")
+console.log("All Might: Let me start by saying I have no idea who this luffy is and what the Strawhat Pirates are. You are on Earth and are wellcomed to UA High School where heroes are trained. You were accepted to UA High School after you cut up a couple of villians that attacked a summer camp that had Hero Course UA students. And You are in Japan. You seem to dress like you are from the past as it does not look like you come from " + worldname + ". You defintaly seem in manner of fighting style alone to not come from" + year + ". May I ask how old you are by chance?")
+console.log("Zoro: I am" + age+ ". It seems like I am not from this world based on what you said so I am going to have to use " + weaponUsed[2] + " to get out of here. Well here goes nothing." )
+console.log("All Might: Hold on you should not go swinging a sword around like that. That could cause serious destruction and injuries.")
+console.log("Zoro: I am getting out of this world and back to mine. I really could not care about property damage and a some people getting injuried. I have no personal stake in this place. And this is not the first time I have used this sword to fight with. If you are going to try to stop me from going home then you will have to fight me, and trust me this will not be an easy fight for you.")
+console.log("All Might: I think you are really under valuing my power, and I will stop you from causing destruction.")
+console.log("Zoro: Fine then. We Will fight. Three sword style onigiri.")
+console.log("All Might: cough. What the heck was that attack. You cut me. These injuries are bad too. How powerful are you? Better yet what are you?")
+console.log("Zoro: The name is " + name + " and I will be the strongest swordsman. I am know to many in my world as," + title + ", but I have long since ended that carrer choice when I joined Luffy as his first crewmate. I think a more apporiate title would be Demon King Zoro. Now then I will be going now.")
+console.log("All Might: Wait. What. How did you cut a hole in the world, and who are those people?")
+console.log("Zoro: I cut a hole in the world because I am Zoro who wields Enma which is for sure my strongest weapon, and those people are my crewmates. Good Bye All Might.")
+console.log("All Might: Your words are " + hasStrongestWeapon + ", Enma is for sure very strong.")
+console.log("Zoro: Good Bye All Might.")
+console.log("The End")
